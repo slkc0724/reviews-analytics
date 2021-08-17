21c32a5f6305107062beda8c99fb1dd5e5b08b50
slkc0724 <88265542+slkc0724@users.noreply.github.com>
2021-08-17 22:59:44 +0800

    Add files via upload

--- r.py
+++ r.py
@@ -7,3 +7,13 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 print('檔案讀取完成, 總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('average is', sum_len/len(data))
+
+# print('------------------')
+# print(data[0])
+# print('------------------')
+# print(data[1]) 
