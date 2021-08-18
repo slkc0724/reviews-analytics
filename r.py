1fc979167ff26429c4f8aa4903c18cf82070fc15
slkc0724 <88265542+slkc0724@users.noreply.github.com>
2021-08-18 23:58:05 +0800

    Add files via upload

--- r.py
+++ r.py
@@ -8,6 +8,11 @@ with open('reviews.txt', 'r') as f:
 			print(len(data))
 print('檔案讀取完成, 總共有', len(data), '筆資料')
 
+# print('------------------')
+# print(data[0])
+# print('------------------')
+# print(data[1]) 
+
 sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
@@ -22,7 +27,10 @@ print('there are', len(new), 'comments which the length is smaller than 100')
 # print(new[0])
 # print(new[1])
 
-# print('------------------')
-# print(data[0])
-# print('------------------')
-# print(data[1]) 
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print(len(good))
+
+# print(good[0])
